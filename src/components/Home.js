557c85d22e78a3fe3f6a33730f1d1da6a65da83f
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2023-09-26 15:46:37 -0400

    Home component updated-content of component added

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home(){
+    return(
+        <div>
+            <h1>Live Performance App</h1>
+        <h2>Use for reference during your practice and Performance</h2>
+        </div>
+    )
+}
+
+export default Home;

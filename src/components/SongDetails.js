43e879107db584b7df7449fdafb33fb136e0e85b
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2023-10-04 16:39:55 -0400

    add useParams hook and variable in SongDetails component

---
+++ src/components/SongDetails.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from "react";
+import {useParams} from "react-router-dom";
+
+function SongDetails(){
+    const [selectedSong, setSelectedSong] = useState();
+    const params = useParams()
+
+    useEffect(()=>{
+        fetch(``)
+    },[params.id])
+
+    return(
+        <h2>Current Song Details</h2>
+    )
+}
+
+export default SongDetails;

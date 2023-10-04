0617f436093327584deed69576ba0f84df7cf869
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2023-10-04 17:42:14 -0400

    SongDetails updated; useEffect fetch added, contents of div added

--- src/components/SongDetails.js
+++ src/components/SongDetails.js
@@ -2,16 +2,24 @@ import React, {useState, useEffect} from "react";
 import {useParams} from "react-router-dom";
 
 function SongDetails(){
-    const [selectedSong, setSelectedSong] = useState();
+    const [selectedSong, setSelectedSong] = useState([]);
     const params = useParams()
 
     useEffect(()=>{
-        fetch(``)
+        fetch(`http://localhost:3000/songs/${params.id}`)
+        .then(resp=>resp.json())
+        .then(songDetails=>setSelectedSong(songDetails))
     },[params.id])
 
     return(
-        <h2>Current Song Details</h2>
-
+        <div>
+            <h2>Current Song Details</h2>
+            <h3>{selectedSong.name}</h3>
+            <h3>{selectedSong.artist}</h3>
+            <h3>{selectedSong.songKey}</h3>
+            <h4>{selectedSong.playerNotes}</h4>
+            <h2>{selectedSong.videoReference}</h2>
+        </div>
     )
 }
 
